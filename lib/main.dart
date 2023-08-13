57e544ee6995f2da80f90fd7b1d4460847cd3cd7
Jeison Poveda <jeisonfpovedac@gmail.com>
2023-08-13 16:19:26 -0500

    Changed - se cambió el nombre de la app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:APP_Characters/presentation/screens/counter/counter_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.epilogueTextTheme(),
+        scaffoldBackgroundColor:
+            const Color(0xFF0D0118), // Establece el color de fondo global aquí
+      ),
+      home: const CounterScreen(),
+    );
+  }
+}
